4e9bf398e35cfb9d371d25c11740b1bf2d5d1ba4
lolletje123 <101660758+lolletje123@users.noreply.github.com>
2022-12-27 00:21:00 +0100

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  env: {
+    Social1: "Steam",
+    link1: "",
+
+    Social2: "Github",
+    link2: "https://github.com/lolletje123",
+
+    Social3: "Discord",
+    link3: "https://discord.com/users/1042530603674898432",
+
+    userName: "Flynn",
+
+    cards: [
+      {
+        title: "flypredictor",
+        link: "https://www.flypredictor.com",
+        role: "Owner",
+        purpose: "E-scooter tune,s",
+        paragraph:
+          "flypredictor.nl is a site where they sell software for your electric scooter to make it go faster.",
+        Project1: "There's no github for this project.",
+        Plink1: "",
+        Project2: "Theres No Discord for this project.",
+        Plink2: "",
+      },
+      {
+        title: "gtaoutfit",
+        link: "",
+        role: "Developer",
+        purpose: "GTA 5 accounts",
+        paragraph:
+          "gtaoutfit.nl is a website where we are selling gta5 modded accounts for ps4/xbox/pc",
+        Project1: "There's no github for this project.",
+        Plink1: "",
+        Project2: "Theres No Discord for this project.",
+        Plink2: "",
+      },
+      {
+        title: "Sakura iland",
+        link: "https://www.roblox.com/games/8610523031/WINTER-Sakura-Island",
+        role: "Owner",
+        purpose: "Roblox game",
+        paragraph:
+          "Sakura iland is a game in roblox where u can vibe with you friends",          Project1: "Kythi",
+        Project1: "There's no github for this project.",
+        Plink1: "",
+        Project2: "Join our discord !",
+        Plink2: "https://discord.gg/bbKjBvVv",
+      },
+    ],
+  },
+};
